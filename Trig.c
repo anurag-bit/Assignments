b8452e03e73811d4284a3a3c5165162c70b69efd
Anurag Kumar Singh <singh.ask@outlook.com>
2023-09-27 13:40:00 +0530

    added Documentation!

--- Trig.c
+++ Trig.c
@@ -4,22 +4,28 @@ Peer Graded Assignment: Design a program to calculate value of cosine and sine f
 https://www.github.com/anurag-bit
 */
 
+
+// Pre_processor directive
 #include<stdio.h>
 #include<math.h>
 
-
+//program entry point!
 int main(){
+// variable declaration 
     double sine;
     double cosine;
 
 printf("\t Sin\t \tCosine \t \n");
+// flow control logic for range definition of input/plugin value of functions, precisely in sin and cos! 
  for ( double i = 0.0; i <= 1; i += 0.1)
  {
+    //iterative execution of instruction-------------->
     double rSin = sin(i);
     double rCos = cos(i);
 printf("\t %lf\t \t%lf \t \n", rSin, rCos);
 
  }
+// program exit point!
 return 0; 
 
 }
