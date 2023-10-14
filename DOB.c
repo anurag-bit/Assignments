4a3fcb0fadec3f3f437884a5722c8f5e7376fc40
Anurag Kumar Singh <singh.ask@outlook.com>
2023-10-14 20:15:30 +0530

    added trial dob program

---
+++ DOB.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    extern int dob[3];
+     extern int  calc_year;
+    {
+        printf("Hello, Please enter you DOB: ");
+        scanf("%d%d%d", &dob);
+    }
+
+    {
+        static int countYear = 0;
+        if(dob[2]: dob[2]==2023;dob[2] = dob[2]++)
+            while (dob[2]<2023)
+            {
+                countYear++;
+            }
+            calc_year = countYear;
+
+    }
+
+    return 0;
+}
